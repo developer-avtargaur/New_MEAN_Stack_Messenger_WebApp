23b8ce680014b0daa9d30101a9a423e9ccba8f54
rollschild <rollschild@gmail.com>
2018-06-20 14:57:14 -0400

    A little bit on Angular front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // our own html tag
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
