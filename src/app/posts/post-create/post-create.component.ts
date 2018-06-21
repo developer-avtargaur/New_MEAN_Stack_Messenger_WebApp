11f7b6de6d63ac87b4d6a9da134f493995a5e2ca
rollschild <rollschild@gmail.com>
2018-06-21 15:24:15 -0400

    Front-end: create posts via event & property binding

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'NO CONTENT';
-  onAddPost(postInput: HTMLTextAreaElement) {
-    console.log(postInput);
-    // console.dir(postInput);
-    this.newPost = postInput.value;
-    alert('Post added!');
-  }
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter(); // turn this into an event
   onAddPostTwoWay() {
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 }
